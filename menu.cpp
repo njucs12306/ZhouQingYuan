d522f2d493719eec5a8ecefc0a482876c1025c39
ZQY181860151 <47181985+ZQY181860151@users.noreply.github.com>
2019-02-15 17:37:12 +0800

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,185 @@
+#include"pch.h"
+using namespace std;
+//主菜单显示函数
+void main_menu()
+{
+	cout << endl << "                                  欢迎使用12306选票系统";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      1 录入班次信息       ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      2 操作班次信息       ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      3 查询班次信息       ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      4 售票与退票系统     ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      5 导出票务系统       ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      6 退出系统           ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+
+}
+//操作班次菜单显示函数
+void menu2()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      1 添加/修改班次      ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      2 删除班次           ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      3 返回主菜单         ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+}
+//班次查询菜单显示函数
+void menu3()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      1 按班次查询         ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      2 按站查询           ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      3 返回主菜单         ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+}
+//票务系统菜单显示函数
+void menu4()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      1 售票               ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      2 订单浏览           ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      3 退票               ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	cout << "：      4 返回主菜单         ：" << endl;
+	for (int i = 0; i < 30; i++)
+	{
+		cout << " ";
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		cout << "*";
+	}
+}
+//添加班次函数（待定）
